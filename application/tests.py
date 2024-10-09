7740aaa3aaf34f22288d1669cb91f923d0a1cfbb
a.burgart02 <a.burgart02@gmail.com>
2024-10-09 23:11:38 +0500

    Fix tests

--- application/tests.py
+++ application/tests.py
@@ -1,7 +1,9 @@
+import datetime
 import random
 import sqlite3
 import string
 import unittest
+import regex
 import requests
 from selenium import webdriver
 
@@ -25,35 +27,45 @@ class BlogTestCase(unittest.TestCase):
         return data
 
     @staticmethod
-    def generate_article(user):
+    def generate_article():
         letters = string.ascii_lowercase
-        data = {
+        article = {
+            'id': random.randint(1000, 2000),
             'title': ''.join(random.choice(letters) for _ in range(10)),
             'text': ''.join(random.choice(letters) for _ in range(10)),
-            'topic': ''.join(random.choice(letters) for _ in range(10)),
-            'user': user
+            'date': datetime.date.today(),
+            'userId_id': 1,
+            'topicId_id': 1
         }
-        return data
+        return article
 
     def test_home_page_title(self):
         self.driver.get(self.url)
-        self.assertIn("Main", self.driver.title)
+        self.assertEqual("Main", self.driver.title)
 
     def test_registration(self):
         url = f'{self.url}/registration'
         user = self.generate_user()
         response = requests.post(url, data=user)
-        self.assertIn(str(response.status_code), "200")
+        self.assertEqual(str(response.status_code), "200")
 
     def test_get_articles(self):
         user = self.generate_user()
         conn = sqlite3.connect('../db.sqlite3')
-        sql = f''' INSERT INTO application_article(id, title, text, date, userId_id, topicId_id) VALUES({random.randint(100, 200)},'title','text','date',1,1) '''
-        cur = conn.cursor()
-        cur.execute(sql, user)
-        conn.commit()
-        article = self.generate_article(user)
-        article_url = f'{self.url}/add-article'
-        article_response = requests.post(article_url, data=article)
-        self.assertIn(str(article_response.status_code), "200")
+        articles = [self.generate_article() for _ in range(1)]
+        for article in articles:
+            sql = f''' INSERT INTO application_article(id, title, text, date, userId_id, topicId_id) VALUES(
+                '{article['id']}','{article['title']}','{article['text']}','{article['id']}','{article['id']}','{article['id']}') '''
+            cur = conn.cursor()
+            cur.execute(sql, user)
+            conn.commit()
+        self.driver.get(self.url)
+        page = self.driver.page_source
+        titles = regex.findall('<a href="\/article\/.*">(.*)</a>', page)
+        for article in articles:
+            sql = f''' DELETE FROM application_article WHERE id = {article['id']} '''
+            cur = conn.cursor()
+            cur.execute(sql, user)
+            conn.commit()
+        self.assertTrue(all(elem in titles for elem in [article['title'] for article in articles]))
 
